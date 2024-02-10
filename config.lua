497499b50f502da0fe86c1b84effed308dae01c2
braidenbannister <159653090+braidenbannister@users.noreply.github.com>
2024-02-10 14:30:59 -0600

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,4 @@
+Cfg             = {}
+Cfg.DiffTrigger = 0.255 
+Cfg.MinSpeed    = 19.25 --THIS IS IN m/s
+Cfg.Strings     = { belt_on = 'Buckled^5 Seatbelt^0.', belt_off = 'Unbuckled ^1 Seatbelt^0.' }
